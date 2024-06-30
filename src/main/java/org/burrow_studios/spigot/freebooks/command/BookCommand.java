e090cb92cfc07df0cd0f64a1be96900f9133252b
nicosleax <forceeax@gmail.com>
2024-06-30 16:15:51 +0200

    BookCommand created

---
+++ src/main/java/org/burrow_studios/spigot/freebooks/command/BookCommand.java
@@ -0,0 +1,42 @@
+package org.burrow_studios.spigot.freebooks.command;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.burrow_studios.spigot.freebooks.Main;
+
+public class BookCommand implements CommandExecutor {
+
+    public Main main;
+
+
+    public BookCommand(Main main){
+        this.main = main;
+    }
+    
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
+        if(!(sender instanceof Player player)){return true;}
+
+        //implement new Item "Writable_Book"
+        ItemStack book = new ItemStack(Material.WRITABLE_BOOK);
+
+        // give "book" a new name
+        ItemMeta bookname = book.getItemMeta();
+        bookname.setDisplayName("Pergament");
+        book.setItemMeta(bookname);
+
+        // give player the item
+        player.getInventory().addItem(book);
+
+
+        player.sendMessage(main.getConfig().getString("ChatPrefix") + "§7Du hast ein Pergament erhalten.");
+        return true;
+        
+    }
+}
