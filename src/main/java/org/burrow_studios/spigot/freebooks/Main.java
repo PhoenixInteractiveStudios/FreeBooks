b8d9e8fa7b6ecb1909227fb3bedc9b6fdc4fe278
TurtleException <turtle.exception@eldritch.de>
2024-08-31 18:20:43 +0200

    Some formatting

--- src/main/java/org/burrow_studios/spigot/freebooks/Main.java
+++ src/main/java/org/burrow_studios/spigot/freebooks/Main.java
@@ -1,28 +1,15 @@
 package org.burrow_studios.spigot.freebooks;
 
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.burrow_studios.spigot.freebooks.command.BookCommand;
 
-
-public class Main extends JavaPlugin implements Listener, CommandExecutor {
-
-    public BookCommand bookcommand;
-
-
+public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-
         // write a config.yml
         saveDefaultConfig();
 
-        // implement Java Class "BookCommand" in "Main"
-        bookcommand = new BookCommand(this);
-
-
-        // implement Command "book"
+        BookCommand bookcommand = new BookCommand(this);
         getCommand("book").setExecutor(bookcommand);
-
     }
 }
--- src/main/java/org/burrow_studios/spigot/freebooks/command/BookCommand.java
+++ src/main/java/org/burrow_studios/spigot/freebooks/command/BookCommand.java
@@ -8,20 +8,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.burrow_studios.spigot.freebooks.Main;
+import org.jetbrains.annotations.NotNull;
 
 public class BookCommand implements CommandExecutor {
+    private final Main main;
 
-    public Main main;
-
-
-    public BookCommand(Main main){
+    public BookCommand(Main main) {
         this.main = main;
     }
-    
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
-        if(!(sender instanceof Player player)){return true;}
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, String[] strings) {
+        if (!(sender instanceof Player player)) return true;
 
         //implement new Item "Writable_Book"
         ItemStack book = new ItemStack(Material.WRITABLE_BOOK);
@@ -34,9 +32,7 @@ public class BookCommand implements CommandExecutor {
         // give player the item
         player.getInventory().addItem(book);
 
-
-        player.sendMessage(main.getConfig().getString("ChatPrefix") + "§7Du hast ein Pergament erhalten.");
+        player.sendMessage(main.getConfig().getString("chatPrefix") + "§7Du hast ein Pergament erhalten.");
         return true;
-        
     }
 }
